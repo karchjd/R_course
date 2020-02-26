ebf13acf4cf41fb6a50b3bb05d081e29d8b85338
Julian Karch <j.d.karch@fsw.leidenuniv.nl>
2020-02-26 21:47:16 +0100

    improved t-test

--- calc_t_test.R
+++ calc_t_test.R
@@ -1,28 +1,32 @@
-library(testthat)
 #' Welch's t-test
 #'
-#' Calculates the Welch t-test
+#' Calculates Welch's t-test
 #'
 #' @param x vector of observations group 1
 #' @param y vector of observations group 2
-#' @details t is the t value
-#'          df is the degrees of freedom
-#'          p is the p-value
-calc_t_test <- function(x, y) {
-  validate_t_input(x,y)
-  
-  
-  # calculate prelim.
-  mean_x <- julian::mean(x)
+#' @param mu mean difference to test against
+#' @return A list with letters and numbers.
+#' \itemize{
+#'   \item t - t-value
+#'   \item df - degrees of freedom
+#'   \item p - p-value
+#' }
+calc_t_test <- function(x, y, mu = 0) {
+  validate_t_input(x, y)
+
+
+  # calculate preliminaries
+  mean_x <- mean(x)
   mean_y <- mean(y)
   var_x <- var(x)
   var_y <- var(y)
   n1 <- length(x)
   n2 <- length(y)
 
+  validate_t_post(var_x, var_y)
+
   # calculate t-value
-  
-  mean_difference <- mean_x - mean_y
+  mean_difference <- mean_x - mean_y - mu
   pooled_variance <- var_x / n1 + var_y / n2
   t <- mean_difference / sqrt(pooled_variance)
 
@@ -33,57 +37,29 @@ calc_t_test <- function(x, y) {
   df_denumer <- var_x^2 / (n1^2 * v_x) + var_y^2 / (n2^2 * v_y)
   df <- df_numer / df_denumer
 
-  # calculate p-value
-  p_val_bigger <- pt(t, df, lower.tail = TRUE)
-  p_val_smaller <- pt(t, df, lower.tail = FALSE)
-  p <- 2 * min(p_val_bigger, p_val_smaller)
-
+  p <- calc_p_val(t,df)
   return(list(t = t, df = df, p = p))
 }
 
-calc_mean <- function(x) {
-  current_sum <- 0
-  for (i in 1:length(x)) {
-    current_sum <- current_sum + x[i]
-  }
-  the_mean <- current_sum / length(x)
-  return(the_mean)
+calc_p_val <- function(t,df){
+  p_val_bigger <- pt(t, df, lower.tail = TRUE)
+  p_val_smaller <- pt(t, df, lower.tail = FALSE)
+  p <- 2 * min(p_val_bigger, p_val_smaller)
+  return(p)
 }
 
-calc_variance <- function(x) {
-  current_SS <- 0
-  our_mean <- calc_mean(x)
-  for (i in 1:length(x)) {
-    current_SS <- current_SS + (x[i] - our_mean)^2
-  }
-  the_var <- current_SS / (length(x)-1)
-  return(the_var)
-}
-
-validate_t_input <- function(x,y){
-  if(class(x) != "numeric" || class(y) != "numeric"){
+validate_t_input <- function(x, y) {
+  if (class(x) != "numeric" || class(y) != "numeric") {
     stop("x and y must be numeric")
   }
-  
-  if(length(x) == 1 || length(y) == 1){
+
+  if (length(x) == 1 || length(y) == 1) {
     stop("x and y must have at least 2 observations")
   }
 }
 
-x <- rnorm(10^7)
-y <- rnorm(10^7)
-
-our_res <- calc_t_test(x, y)
-r_res <- t.test(x, y)
-
-expect_equivalent(our_res$df,r_res$parameter)
-expect_equivalent(our_res$p,r_res$p.value)
-expect_equivalent(our_res$t,r_res$statistic)
-
-expect_equivalent(calc_mean(x),mean(x))
-expect_equivalent(calc_variance(x),var(x))
-
-expect_error(calc_t_test("Hallo","Toto"),"x and y must be numeric")
-expect_error(calc_t_test(10,5),"x and y must have at least 2 observations")
-expect_error(calc_t_test(julian,david),"x and y must be numeric")
-
+validate_t_post <- function(var_x, var_y) {
+  if (var_x == 0 || var_y == 0) {
+    stop("data are essentially constant")
+  }
+}
---
+++ test_calc_t_test.R
@@ -0,0 +1,22 @@
+library(testthat)
+this.dir <- dirname(parent.frame(2)$ofile)
+setwd(this.dir)
+source("calc_t_test.R")
+x <- rnorm(10^3)
+y <- rnorm(10^3)
+
+our_res <- calc_t_test(x, y)
+r_res <- t.test(x, y)
+
+expect_equivalent(our_res$df, r_res$parameter)
+expect_equal(our_res$p, r_res$p.value)
+expect_equivalent(our_res$t, r_res$statistic)
+
+our_res <- calc_t_test(x, y, mu = 4)
+r_res <- t.test(x, y, mu = 4)
+expect_equal(our_res$p, r_res$p.value)
+
+expect_error(calc_t_test("Hallo", "Toto"), "x and y must be numeric")
+expect_error(calc_t_test(10, 5), "x and y must have at least 2 observations")
+expect_error(calc_t_test(julian, david), "x and y must be numeric")
+expect_error(calc_t_test(rep(1, 10), rep(2, 10)), "data are essentially constant")
